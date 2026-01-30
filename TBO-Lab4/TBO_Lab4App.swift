82962bf9842ffd2ad5410a753207c8b59c676218
Michał Fereniec <michalkoks99@gmail.com>
2026-01-30 20:14:46 +0100

    Initial Commit

---
+++ TBO-Lab4/TBO_Lab4App.swift
@@ -0,0 +1,17 @@
+//
+//  TBO_Lab4App.swift
+//  TBO-Lab4
+//
+//  Created by Michal Fereniec on 30/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct TBO_Lab4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TBO-Lab4Tests/TBO_Lab4Tests.swift
@@ -0,0 +1,17 @@
+//
+//  TBO_Lab4Tests.swift
+//  TBO-Lab4Tests
+//
+//  Created by Michal Fereniec on 30/01/2026.
+//
+
+import Testing
+@testable import TBO_Lab4
+
+struct TBO_Lab4Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
